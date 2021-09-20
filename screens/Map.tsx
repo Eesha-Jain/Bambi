fc55e4563cc1b77f1a2e73c12a6108aaa3c4f0ee
Eesha Jain <68827992+Eesha-Jain@users.noreply.github.com>
2021-09-19 19:51:12 -0700

    Integrated map view - no markers yet

--- screens/Map.tsx
+++ screens/Map.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import { StyleSheet, Text, View, Dimensions, Image, TouchableOpacity } from 'react-native';
 const win = Dimensions.get('window');
 import colors from '../assets/files/Colors';
+import MapView from 'react-native-maps';
 
 export default function Map({navigation: {navigate}}) {
   return (
     <View style={styles.container}>
-      <Text style={{fontFamily: "ns-light", fontSize: 16, color: colors.brown, marginTop: 20, marginLeft: 20, marginRight: 20, textAlign: "center"}}>BAMBI</Text>
+        <View style={{paddingTop: 25}}><Image source={require('../assets/images/Top.png')} style={{width: win.width, height: win.width * (119 / 1242)}} /></View>
+        <MapView style={styles.map} />
+
+        <TouchableOpacity style={{marginTop: 20, backgroundColor: colors.yellow, padding: 20, width: win.width * 0.9, borderRadius: 10}}><Text style={{color: colors.brown, fontFamily: "ns-regular", fontSize: 14, textAlign: "center"}}>REPORT DEER CROSSING AT YOUR LOCATION</Text></TouchableOpacity>
     </View>
   );
 }
@@ -17,11 +21,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  topImage: {
-    width: win.width * 0.8,
-    height: win.width * 0.8,
-    marginTop: 30
-  },
+  map: {
+    marginTop: 20,
+    width: Dimensions.get('window').width * 0.9,
+    height: Dimensions.get('window').height * 0.73,
+  }
 });
