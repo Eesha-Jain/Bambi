4ba266dfbf121a3cd16f431a88afd238e7164ba1
Eesha Jain <68827992+Eesha-Jain@users.noreply.github.com>
2021-09-19 21:27:55 -0700

    Added sample marker to map

--- screens/Map.tsx
+++ screens/Map.tsx
@@ -1,15 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Dimensions, Image, TouchableOpacity } from 'react-native';
 const win = Dimensions.get('window');
 import colors from '../assets/files/Colors';
 import MapView from 'react-native-maps';
+import data from '../tempmapdata.ts';
 
 export default function Map({navigation: {navigate}}) {
+  const [reports, setReports] = useState([]);
+
+  useEffect(() => {
+    setReports(data.reports);
+  }, []);
+
   return (
     <View style={styles.container}>
         <View style={{paddingTop: 25}}><Image source={require('../assets/images/Top.png')} style={{width: win.width, height: win.width * (119 / 1242)}} /></View>
-        <MapView style={styles.map} />
+
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.1,
+            longitude: -95.7,
+            latitudeDelta: 10,
+            longitudeDelta: 45
+          }} >
+            {
+              reports.map((report) => <MapView.Marker
+                key={report.id}
+                coordinate={{ latitude: report.lat, longitude: report.lon }}
+                title={report.location}
+                description={report.comments}
+              >
+              </MapView.Marker>)
+            }
+        </MapView>
 
         <TouchableOpacity style={{marginTop: 20, backgroundColor: colors.yellow, padding: 20, width: win.width * 0.9, borderRadius: 10}}><Text style={{color: colors.brown, fontFamily: "ns-regular", fontSize: 14, textAlign: "center"}}>REPORT DEER CROSSING AT YOUR LOCATION</Text></TouchableOpacity>
     </View>
---
+++ tempmapdata.ts
@@ -0,0 +1,13 @@
+export default {
+  "count": 1,
+  "reports": [
+    {
+      "id": 45981,
+      "location": "DEER SIGHTING",
+      "lat": 36.62,
+      "lon": -85.73,
+      "comments": "4 minutes ago",
+      "created_at": "2020-03-05T14:54:06.650Z",
+    },
+  ]
+}
