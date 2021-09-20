d878c464395f8007262823999ad9be70e5781fdf
Eesha Jain <68827992+Eesha-Jain@users.noreply.github.com>
2021-09-19 21:58:38 -0700

    Updated tempmapdata

--- tempmapdata.ts
+++ tempmapdata.ts
@@ -4,8 +4,8 @@ export default {
     {
       "id": 45981,
       "location": "DEER SIGHTING",
-      "lat": 36.62,
-      "lon": -85.73,
+      "lat": 47.624698338494774,
+      "lon": -122.12536601110386,
       "comments": "4 minutes ago",
       "created_at": "2020-03-05T14:54:06.650Z",
     },
